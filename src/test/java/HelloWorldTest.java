86a29c871439ceef8323809a5acad0f26771ffb0
Little Mike <code@littlemike.co.uk>
2017-10-04 21:26:48 +0200

    Initial empty project commit

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloWorldTest {
+
+    @Test
+    public void greetingShouldBeHelloWorld() {
+        HelloWorld greeter = new HelloWorld();
+        String greeting = greeter.getGreeting();
+        assertThat(greeting)
+                .isEqualTo("Hello world!");
+    }
+}
