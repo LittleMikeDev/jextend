f04d4ec90100259405c7a64a0072532d8d18e1a0
Little Mike <code@littlemike.co.uk>
2017-11-03 16:31:28 +0100

    Add skeleton structure for jdk implementation & generic tests

---
+++ jextend-core/src/test/java/uk/co/littlemike/jextend/impl/BaseExtensionTest.java
@@ -0,0 +1,11 @@
+package uk.co.littlemike.jextend.impl;
+
+import org.junit.Test;
+
+public abstract class BaseExtensionTest {
+
+    @Test
+    public void testTest() {
+        System.out.println("Hello world!");
+    }
+}
---
+++ jextend-jdk/src/test/java/uk/co/littlemike/jextend/impl/jdk/JdkExtensionTest.java
@@ -0,0 +1,6 @@
+package uk.co.littlemike.jextend.impl.jdk;
+
+import uk.co.littlemike.jextend.impl.BaseExtensionTest;
+
+public class JdkExtensionTest extends BaseExtensionTest {
+}
