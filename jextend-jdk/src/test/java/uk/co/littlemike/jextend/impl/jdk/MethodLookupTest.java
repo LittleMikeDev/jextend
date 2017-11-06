6a61d66c9990696e63181ce1d13c24120d4814ff
Little Mike <code@littlemike.co.uk>
2017-11-06 21:03:46 +0100

    Add test for privileged private method lookup failure

--- jextend-jdk/src/test/java/uk/co/littlemike/jextend/impl/jdk/MethodLookupTest.java
+++ jextend-jdk/src/test/java/uk/co/littlemike/jextend/impl/jdk/MethodLookupTest.java
@@ -8,9 +8,16 @@ public class MethodLookupTest {
     }
 
     @Test(expected = MethodLookupException.class)
-    public void throwsExceptionWhenAttemptingToLookupPrivateMethod() throws Exception {
+    public void throwsExceptionWhenAttemptingToLookupPrivateMethodOnDifferentClass() throws Exception {
         MethodLookup lookup = new MethodLookup(Object.class);
 
         lookup.lookup(MethodLookupTest.class.getDeclaredMethod("privateMethod"));
+    }
+
+    @Test(expected = MethodLookupException.class)
+    public void throwsExceptionWhenAttemptingToLookupPrivateMethodOnDifferentClass_EvenIfPrivileged() throws Exception {
+        PrivilegedMethodLookup lookup = new PrivilegedMethodLookup(Object.class);
+
+        lookup.lookupDefault(MethodLookupTest.class.getDeclaredMethod("privateMethod"));
     }
 }
