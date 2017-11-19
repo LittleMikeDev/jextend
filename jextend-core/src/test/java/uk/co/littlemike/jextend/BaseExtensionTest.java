d5691c895dbd62d5e0d54e3690659e6f0256941a
Little Mike <code@littlemike.co.uk>
2017-11-19 20:57:07 +0100

    Add additional test cases to cover more expected behaviour
    - Extension methods can call delegated methods
    - Delegated methods *can not* call extension methods

--- jextend-core/src/test/java/uk/co/littlemike/jextend/BaseExtensionTest.java
+++ jextend-core/src/test/java/uk/co/littlemike/jextend/BaseExtensionTest.java
@@ -40,6 +40,8 @@ public abstract class BaseExtensionTest {
         default String greeting() {
             return "Hello world!";
         }
+
+        default String sizeDescription() { return "My size is " + size(); }
     }
 
     @Test
@@ -47,6 +49,42 @@ public abstract class BaseExtensionTest {
         TalkingList talkingList = extendListWith(TalkingList.class);
 
         assertThat(talkingList.greeting()).isEqualTo("Hello world!");
+    }
+
+    @Test
+    public void defaultMethodMayCallDelegateMethod() {
+        TalkingList talkingList = extendListWith(TalkingList.class);
+        talkingList.add("Item");
+
+        assertThat(talkingList.sizeDescription()).isEqualTo("My size is 1");
+    }
+
+    public interface Base {
+        default String getName() {
+            return "Base";
+        }
+
+        default String sayName() {
+            return "I am " + getName();
+        }
+    }
+
+    public interface Extension extends Base {
+        @Override
+        default String getName() {
+            return "Extension";
+        }
+    }
+
+    @Test
+    public void delegatedMethodCallDoesNotCallOverriddenMethod() {
+        Base base = new Base() {};
+        Extension extension = JExtend.getExtension(Base.class, Extension.class).extend(base);
+
+        assertThat(base.getName()).isEqualTo("Base");
+        assertThat(base.sayName()).isEqualTo("I am Base");
+        assertThat(extension.getName()).isEqualTo("Extension");
+        assertThat(extension.sayName()).isEqualTo("I am Base"); // NOTE!
     }
 
     private <E extends List> E extendListWith(Class<E> extensionInterface) {
